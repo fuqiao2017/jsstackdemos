f39485032e985ea70b167e1616cbb0c1b885aa72
fuqiao2017 <270217737@qq.com>
2018-01-28 16:24:51 +0700

    55

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,5 +1,9 @@
 // @flow
 
-export const WEB_PORT = process.env.PORT || 8080
+export const WEB_PORT = 7000
+export const WDS_PORT = 8086
 export const STATIC_PATH = './public'
 export const APP_NAME = 'HELLO APP AAA'
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
